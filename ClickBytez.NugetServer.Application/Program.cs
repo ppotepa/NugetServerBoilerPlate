50abbfa0443c0124622af9ea1deae4e836280571
PGS-SOFT\ppotepa <ppotepa@pgs-soft.com>
2022-03-27 14:47:08 +0200

    Add project files.

---
+++ ClickBytez.NugetServer.Application/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+namespace ClickBytez.NugetServer.Application
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
